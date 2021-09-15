c27257fcae4f0af2f29725208a465bb1b3adec29
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-09-15 02:23:59 +0000

    pizza

---
+++ constants.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Created by: Haokai Li
+# Created on: Sept 2021
+# This Program  is library of calculate the total cost of pizza
+
+LABOR = 0.75
+RENT = 1
+COST_PER_INCH = 0.5
+HST = 1.13
---
+++ pizza.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Created by: Haokai Li
+# Created on: Sept 2021
+# This Program calculate the total cost of pizza
+
+import constants
+
+
+def main():
+    # This function calculate the total cost of pizza
+
+    # input
+    diameter = int(input("Enter diameter of the pizza you would like(inch): "))
+
+    # process
+    sub_total = (
+        constants.LABOR + constants.RENT + (diameter * constants.COST_PER_INCH)
+    )
+    total = sub_total * constants.HST
+
+    # output
+    print("")
+    print("The cost for a {0} inch pizza is ${1:,.2f}".format(diameter, total))
+    print("\nDone")
+
+
+if __name__ == "__main__":
+    main()
